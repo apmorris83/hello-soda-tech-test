a69cdd60853d2b43560535245cdd8b3b613ee9b9
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-22 11:09:57 +0100

    basic rendering of jsonData

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import jsonData from '../../public/data/people.json';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = jsonData;
+    }
     render() {
+        console.log(jsonData);
         return (
             <div>
                 <Header />
-                <h1>Hello</h1>
+                <h1>{this.state.people[0].personal.first_name}</h1>
             </div>
         );
     }
