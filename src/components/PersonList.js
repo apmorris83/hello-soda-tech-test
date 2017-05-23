881f74b640b5e77d0ce9390f793cc488f0669c32
Sarah Jane Morris <apmorris83@gmail.com>
2017-05-23 11:27:17 +0100

    basic profile data rendering with collapsible panel

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,47 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import '../css/PersonList.css';
+
 // import PersonCard from './PersonCard';
 
 function PersonList (props) {
     return (
-
-
-
-
-
-
-
-
-<div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-    {props.people.map((person) => {
-  return ( 
-  <div key={person.id} className="panel panel-default">
-    <div className="panel-heading" role="tab" id={person.id}>
-      <h4 className="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href={person.id} aria-expanded="true" aria-controls="collapseOne">
-          {person.personal.first_name}
-        </a>
-      </h4>
-    </div>
-    <div id={person.id} className="panel-collapse collapse in" role="tabpanel" aria-labelledby={person.id}>
-      <div className="panel-body">
-        {person.devices.map((device, id) => {
-            return (<p key={id}>{device.name}</p>);
-        })}
-      </div>
-    </div>
-  </div>
-  );
-})}
-</div>
-
-
-
-
-
-
-
+        <div className="container">
+            {props.people.map((person, id) => {
+                return (
+                    <div className="media box-element" key={person.id} >
+                        <div className="media-left">
+                            <img className="profile-pic" src={require(`../../public/images/${person.personal.avatar}`)} alt="profile"/>
+                            </div>
+                            <div className="media-body">
+                            <h4>{person.personal.first_name} {person.personal.last_name}, {person.personal.age}</h4>
+                            <h5>{person.personal.job_title}, {person.personal.job_company}</h5>
+                            <h5>{person.personal.city}, {person.personal.country}</h5>
+                            <a className="btn btn-primary pull-right" role="button" data-toggle="collapse" data-target={`#${id}`} aria-expanded="false" aria-controls={`${id}`}>
+                            toggle data
+                            </a>
+                            </div>
+                            <div className="collapse" id={`${id}`}>
+                                <div className="well">
+                                    {person.personal.first_name}
+                                </div>
+                            </div>
+                        </div>
+                    
+                );
+                })}    
+        </div>
     );
 }
 
